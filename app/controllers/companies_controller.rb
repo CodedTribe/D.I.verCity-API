3804dcb8e03f5a363fccb9fe59a7c9870fdd5a86
emerykurt <emerylumsden@gmail.com>
2020-09-20 14:22:23 -0400

    initial commit WIP

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,13 @@
+class CompaniesController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
---
+++ db/migrate/20200920172931_create_companies.rb
@@ -0,0 +1,13 @@
+class CreateCompanies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :hq
+      t.string :logo
+      t.float :leadDemographic
+      t.float :hqDemographic
+
+      t.timestamps
+    end
+  end
+end
